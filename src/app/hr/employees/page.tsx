da4bd99a56ff2d49316ae5629959fa478f69feed
Gilbert Twesigomwe <gilbert@bposeats.com>
2023-07-27 13:59:08 +0300

    list of employees using useQuery

--- src/app/hr/employees/page.tsx
+++ src/app/hr/employees/page.tsx
@@ -1,11 +1,46 @@
 'use client'
 
-import React from 'react';
+import axios from 'axios';
+import React, { useState } from 'react';
+import { useQuery } from '@tanstack/react-query';
+import { Table } from 'antd';
 
-const Overview = () => (
-  <div>
-        Overview
-  </div>
-);
+function Overview() {
+  const [loading, setLoading] = useState(false)
+
+  const columns = [
+    {
+      title: 'First Name',
+      dataIndex: 'first_name',
+      key: 'first_name',
+    },
+    {
+      title: 'Last Name',
+      dataIndex: 'last_name',
+      key: 'last_name',
+    },
+    {
+      title: 'Email',
+      dataIndex: 'email',
+      key: 'email',
+    },
+  ];
+
+  async function getEmployees() {
+    const response = await axios.get('http://localhost:8000/api/employees/')
+    console.log('response/: ', response.data)
+    return response.data
+  }
+
+  const { data: employees, error, isFetching, isLoading } = useQuery({queryKey: ['employees'], queryFn: getEmployees})
+
+  if (error) return (<h1> This is the error: {error.message}</h1>)
+
+  return (
+    <>
+      <Table dataSource={employees} columns={columns} loading={isFetching} />;
+    </>
+  );
+};
 
 export default Overview;
