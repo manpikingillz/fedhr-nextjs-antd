e7cc592d45630a42866b942350e0ddd1266297ac
Gilbert Twesigomwe <gilbert@bposeats.com>
2023-08-08 06:29:25 +0300

    add fetching of notes

--- src/app/hr/employees/employee-details/employee-details-tabs/notes/page.tsx
+++ src/app/hr/employees/employee-details/employee-details-tabs/notes/page.tsx
@@ -1,46 +1,72 @@
-'use client'
+'use client';
 
-import { SettingOutlined } from '@ant-design/icons';
+import { DeleteOutlined } from '@ant-design/icons';
 import React, { useState } from 'react';
 import type { CollapseProps } from 'antd';
-import { Card, Col, Collapse, Divider, Popconfirm, Row, Image } from 'antd';
-
+import { Card, Col, Collapse, Divider, Popconfirm, Row } from 'antd';
+import axios from '@/utils/axios';
+import { useQuery } from '@tanstack/react-query';
+import moment from 'moment';
+import Image from 'next/image'
 
 function Notes() {
 
-  const onChange = (key: string | string[]) => {
-    console.log(key);
-  };
+  async function getNotes() {
+    const response = await axios.get('notes/')
+    return response.data
+  }
 
-  const genExtra = () => (
-    <SettingOutlined
-      onClick={(event) => {
-        // If you don't want click extra trigger collapse, you can prevent this:
-        event.stopPropagation();
-      }}
-    />
-  );
+  const {
+    data: notes,
+    error: errorNotes,
+    isFetching: isFetchingNotes,
+    isLoading: isLoadingNotes,
+    status: statusNotes
+  } = useQuery({queryKey: ['notes'], queryFn: getNotes})
 
-  const items: CollapseProps['items'] = [
-    {
-      key: '1',
-      label: 'Notes',
-      children: <div>Test</div>,
-      extra: genExtra(),
-    }
-  ];
-
-
+  if (errorNotes) return (<h1> This is the error: {errorNotes.message}</h1>)
   return (
     <div>
-      <Collapse
-        defaultActiveKey={['1']}
-        onChange={onChange}
-        expandIconPosition="end"
-        items={items}
-      />
+      <Card style={{ backgroundColor: '#fffff', borderColor: '#ffffff' }}>
+        {notes?.map(note => (
+          <div key={note.id}>
+            <Row>
+              <Col span={2}>
+                <Image src="/images/user_profile.jpg" alt="User Avatar" height={50} width={50}/>
+              </Col>
+              <Col span={21}>
+                <div>
+                  <span style={{ fontWeight: 'bold' }}>
+                    {/* {note.postedBy.firstName + ' ' + note.postedBy.lastName} */}
+                    Gilbert Twesigomwe
+                  </span>
+                  <br />
+                  <span style={{ color: 'lightgrey' }}>
+                    {moment(note.created_at).format('ll')} at{' '}
+                    {moment(note.created_at, 'hh:mm').format('hh:mm A')}
+                  </span>
+                  <br />
+                  <span>{note.note}</span>
+                </div>
+              </Col>
+              <Col span={1}>
+                {/* <Icon type="edit" style={{ color: 'gray' }} /> &nbsp; */}
+                <Popconfirm
+                  title="Sure to delete?"
+                  onConfirm={() => null}
+                >
+                  <a href="javascript:;">
+                    <DeleteOutlined />
+                  </a>
+                </Popconfirm>
+              </Col>
+            </Row>
+            <Divider />
+          </div>
+        ))}
+      </Card>
     </div>
   );
-};
+}
 
 export default Notes;
