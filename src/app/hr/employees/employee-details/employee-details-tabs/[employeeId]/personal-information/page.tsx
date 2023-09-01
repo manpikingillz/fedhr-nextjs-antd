fd2ce795c062d479ed456c8937e93b8c54e801e3
Gilbert Twesigomwe <gilbert@bposeats.com>
2023-09-01 20:25:50 +0300

    added ui for basic info, contact, address, and social links

--- src/app/hr/employees/employee-details/employee-details-tabs/[employeeId]/personal-information/page.tsx
+++ src/app/hr/employees/employee-details/employee-details-tabs/[employeeId]/personal-information/page.tsx
@@ -154,27 +154,96 @@ function PersonalInformation() {
           </div>
         </Form> */}
 
-      <Card title="Contact Info" bordered={true} size="small">
+      <Card title="Contact Info" bordered={true} size="small" headStyle={{backgroundColor: '#F2F2F2'}} className='mt-3'>
+        <div className="flex">
+          <div className='w-1/2'>
+            <div className="flex mb-2">
+              <strong className="	 flex-1 p-1 w-1/3">Work Phone:</strong>
+              <span className="flex-2 p-1 w-2/3 bg-gray-50">+256 780 384 234</span>
+            </div>
+            <div className="flex mb-2">
+              <strong className=" flex-1 p-1  w-1/3">Mobile Phone:</strong>
+              <span className="flex-2 p-1 w-2/3 bg-gray-50">+256 789 374 384</span>
+            </div>
+            <div className="flex mb-2">
+              <strong className=" flex-1 p-1 w-1/3">Home Phone:</strong>
+              <span className="flex-2 p-1 w-2/3 bg-gray-50">+257 703 233 842</span>
+            </div>
+          </div>
+          <div className='w-1/2'>
+            <div className="flex mb-2">
+              <strong className="	 flex-1 p-1 w-1/3">Work Email:</strong>
+              <span className="flex-2 p-1 w-2/3 bg-gray-50">johndoe@company.com</span>
+            </div>
+            <div className="flex mb-2">
+              <strong className=" flex-1 p-1  w-1/3">Home Email:</strong>
+              <span className="flex-2 p-1 w-2/3 bg-gray-50">johndoe@gmail.com</span>
+            </div>
+          </div>
+        </div>
+      </Card>
+      <Card title="Address Info" bordered={true} size="small" headStyle={{backgroundColor: '#F2F2F2'}} className='mt-3'>
+        <div className="flex">
+          <div className='w-1/2'>
+            <div className="flex mb-2">
+              <strong className="	 flex-1 p-1 w-1/3">Street 1:</strong>
+              <span className="flex-2 p-1 w-2/3 bg-gray-50">Ring Road, Kulambiro</span>
+            </div>
+            <div className="flex mb-2">
+              <strong className=" flex-1 p-1  w-1/3">Street 2:</strong>
+              <span className="flex-2 p-1 w-2/3 bg-gray-50">+Kisasi:</span>
+            </div>
+            <div className="flex mb-2">
+              <strong className=" flex-1 p-1 w-1/3">City:</strong>
+              <span className="flex-2 p-1 w-2/3 bg-gray-50">Kampala</span>
+            </div>
+          </div>
+          <div className='w-1/2'>
+            <div className="flex mb-2">
+              <strong className="	 flex-1 p-1 w-1/3">Province:</strong>
+              <span className="flex-2 p-1 w-2/3 bg-gray-50">Central Region</span>
+            </div>
+            <div className="flex mb-2">
+              <strong className=" flex-1 p-1  w-1/3">Zip Code:</strong>
+              <span className="flex-2 p-1 w-2/3 bg-gray-50">00256</span>
+            </div>
+            <div className="flex mb-2">
+              <strong className=" flex-1 p-1  w-1/3">Country:</strong>
+              <span className="flex-2 p-1 w-2/3 bg-gray-50">Uganda</span>
+            </div>
+          </div>
+        </div>
+      </Card>
+      <Card title="Social Links" bordered={true} size="small" headStyle={{backgroundColor: '#F2F2F2'}} className='mt-3'>
+        <div className="flex">
+          <div className='w-1/2'>
+            <div className="flex mb-2">
+              <strong className="	 flex-1 p-1 w-1/3">LinkedIn:</strong>
+              <span className="flex-2 p-1 w-2/3 bg-gray-50">https://www.linkedin.com/in/john-doe/</span>
+            </div>
+            <div className="flex mb-2">
+              <strong className=" flex-1 p-1  w-1/3">Facebook:</strong>
+              <span className="flex-2 p-1 w-2/3 bg-gray-50">https://www.facebook.com/john-doe</span>
+            </div>
+          </div>
+          <div className='w-1/2'>
+            <div className="flex mb-2">
+              <strong className="	 flex-1 p-1 w-1/3">Twitter:</strong>
+              <span className="flex-2 p-1 w-2/3 bg-gray-50">@johndoe</span>
+            </div>
+            <div className="flex mb-2">
+              <strong className=" flex-1 p-1  w-1/3">Instagram:</strong>
+              <span className="flex-2 p-1 w-2/3 bg-gray-50">@johndoe</span>
+            </div>
+          </div>
+        </div>
+      </Card>
+      <Card title="Education" bordered={true} size="small" headStyle={{backgroundColor: '#F2F2F2'}} className='mt-3'>
         <p>Card content</p>
         <p>Card content</p>
         <p>Card content</p>
       </Card>
-      <Card title="Address Info" bordered={true} size="small">
-        <p>Card content</p>
-        <p>Card content</p>
-        <p>Card content</p>
-      </Card>
-      <Card title="Social Links" bordered={true} size="small">
-        <p>Card content</p>
-        <p>Card content</p>
-        <p>Card content</p>
-      </Card>
-      <Card title="Education" bordered={true} size="small">
-        <p>Card content</p>
-        <p>Card content</p>
-        <p>Card content</p>
-      </Card>
-      <Card title="Visa Information" bordered={true} size="small">
+      <Card title="Visa Information" bordered={true} size="small" headStyle={{backgroundColor: '#F2F2F2'}} className='mt-3'>
         <p>Card content</p>
         <p>Card content</p>
         <p>Card content</p>
