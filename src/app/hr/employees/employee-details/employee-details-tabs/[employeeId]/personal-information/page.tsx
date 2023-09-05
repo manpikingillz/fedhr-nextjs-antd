023b3b68e69e6c0082f53f32b859f8d31b0fada6
Gilbert Twesigomwe <gilbert@bposeats.com>
2023-09-05 12:51:57 +0300

    extract personal information display to its component

--- src/app/hr/employees/employee-details/employee-details-tabs/[employeeId]/personal-information/page.tsx
+++ src/app/hr/employees/employee-details/employee-details-tabs/[employeeId]/personal-information/page.tsx
@@ -11,6 +11,7 @@ import { useParams } from 'next/navigation';
 import VisaInformation from './visa/Visa';
 import * as dayjs from 'dayjs'
 import { PersonalInformationForm } from './PersonalInformationForm';
+import { PersonalInformationView } from './PersonalInformationView';
 
 const { Option } = Select;
 
@@ -38,64 +39,7 @@ function PersonalInformation() {
         size="small"
         headStyle={{ backgroundColor: '#F2F2F2' }}
       >
-        {/* 
-        <div className="flex">
-          <div className="w-1/2">
-            <div className="flex mb-2">
-              <strong className="	 flex-1 p-1 w-1/3">First Name:</strong>
-              <span className="flex-2 p-1 w-2/3 bg-gray-50">{employee?.first_name}</span>
-            </div>
-            <div className="flex mb-2">
-              <strong className=" flex-1 p-1  w-1/3">Middle Name:</strong>
-              <span className="flex-2 p-1 w-2/3 bg-gray-50">{employee?.middle_name}</span>
-            </div>
-            <div className="flex mb-2">
-              <strong className=" flex-1 p-1 w-1/3">Last Name:</strong>
-              <span className="flex-2 p-1 w-2/3 bg-gray-50">{employee?.last_name}</span>
-            </div>
-            <div className="flex mb-2">
-              <strong className=" flex-1 p-1 w-1/3">Preferred Name:</strong>
-              <span className="flex-2 p-1 w-2/3 bg-gray-50">{employee?.preferred_name}</span>
-            </div>
-            <div className="flex mb-2">
-              <strong className=" flex-1 p-1 w-1/3">Gender:</strong>
-              <span className="flex-2 p-1 w-2/3 bg-gray-50">{employee?.gender}</span>
-            </div>
-            <div className="flex mb-2">
-              <strong className=" flex-1 p-1 w-1/3">Date of Birth:</strong>
-              <span className="flex-2 p-1 w-2/3 bg-gray-50">{dayjs(employee?.date_of_birth).format('MMM D, YYYY')}</span>
-            </div>
-          </div>
-
-          <div className="w-1/2">
-            <div className="flex mb-2">
-              <strong className=" flex-1 p-1 w-1/3">Marital Status:</strong>
-              <span className="flex-2 p-1 w-2/3 bg-gray-50">{employee?.marital_status}</span>
-            </div>
-            <div className="flex mb-2">
-              <strong className=" flex-1 p-1 w-1/3">Nationality:</strong>
-              <span className="flex-2 p-1 w-2/3 bg-gray-50">{employee?.nationality}</span>
-            </div>
-            <div className="flex mb-2">
-              <strong className=" flex-1 p-1 w-1/3">
-                Social Security Number:
-              </strong>
-              <span className="flex-2 p-1 w-2/3 bg-gray-50">{employee?.social_security_number}</span>
-            </div>
-            <div className="flex mb-2">
-              <strong className=" flex-1 p-1 w-1/3">
-                National Identification Number:
-              </strong>
-              <span className="flex-2 p-1 w-2/3 bg-gray-50">{employee?.national_identification_number}</span>
-            </div>
-            <div className="flex mb-2">
-              <strong className=" flex-1 p-1 w-1/3">
-                Tax Identification Number:
-              </strong>
-              <span className="flex-2 p-1 w-2/3 bg-gray-50">{employee?.tax_identification_number}</span>
-            </div>
-          </div>
-        </div> */}
+        <PersonalInformationView employee={employee}/>
         <PersonalInformationForm />
       </Card>
       
