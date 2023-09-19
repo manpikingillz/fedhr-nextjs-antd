116484e72b3544109a2db3824215a32f0803940e
Gilbert Twesigomwe <gilbert@bposeats.com>
2023-09-19 14:03:34 +0300

    #39 clean up the code abit for inserting text in editor

--- src/app/hr/recruitment/contract_templates/page.tsx
+++ src/app/hr/recruitment/contract_templates/page.tsx
@@ -9,13 +9,30 @@ import TaskList from '@tiptap/extension-task-list';
 import TextStyle from '@tiptap/extension-text-style';
 import { EditorContent, useEditor } from '@tiptap/react';
 import StarterKit from '@tiptap/starter-kit';
-import React, { useCallback, useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import MenuBar from './MenuBar';
 
-const CustomEditor = () => {
-  const [editorContent, setEditorContent] = useState('Hello <mark>{{fullName}}</mark>. This is my Initial Content'); // Initialize the state
+type TemplateField = {
+    fieldNname?: string,
+    displayNname: string,
+    description: string
+}
 
-  const fullName = 'John Doe'
+const fields = [
+    {
+        field_name: 'full_name',
+        display_name: 'Full Name',
+        description: 'Full name of the employee'
+    },
+    {
+        field_name: 'job_position',
+        display_name: 'Job Position',
+        description: 'Job position of the employee'
+    }
+]
+
+const CustomEditor = () => {
+  const [editorContent, setEditorContent] = useState('Hello <mark>{{Full Name}}</mark>. This is my Initial Content'); // Initialize the state
 
   const editor = useEditor({
     extensions: [
@@ -28,14 +45,12 @@ const CustomEditor = () => {
       TextStyle,
       CharacterCount.configure({
         limit: 10000,
-      }),
+      })
     ],
-    content: editorContent//`<h1>Hello {{fullName}}. This is my Initial Content</h1>`,
+    content: editorContent
   });
 
   useEffect(() => {
-    console.log('editor:::: ', editorContent);
-
     if (editor) {
       const updateContent = () => {
         setEditorContent(editor.getHTML());
@@ -47,60 +62,39 @@ const CustomEditor = () => {
         editor.off('update', updateContent);
       };
     }
-    console.log('contettt:::: ', editorContent);
   }, [editor]);
 
   useEffect(() => {
     console.log('content:::: ', editorContent);
   }, [editorContent]);
 
-    const replaceData = ({field_name, display_name}: {field_name?: string, display_name?: string}) => {
-        console.log('replace data');
-        const updatedContent = editorContent.replace('{{fullName}}', fullName);
-        console.log('updatedContent:::: ', updatedContent)
-        if (editor) {
-            editor.chain().setContent(updatedContent).run(); // Set the updated content inside the editor
-            console.log('edito updated:::: ', editor.getHTML())
-        }
-        // if (editor) {
-        //     // Parse the updated content into a ProseMirror Node
-        //     const contentNode = editor.parser.parse(updatedContent);
-    
-        //     // Apply a transaction to replace the content
-        //     editor.view.dispatch(
-        //         editor.view.state.tr.replaceWith(0, editor.view.state.doc.content.size, contentNode)
-        //     );
-        // }
+    const replaceTemplateFieldsWithActualData = (fieldDisplayName: string, fieldData: string) => {
+        const updatedContent = editorContent.replace(`{{${fieldDisplayName}}}`, fieldData);
+
+        // Set the updated content inside the editor
+        if (editor) editor.chain().setContent(updatedContent).run();
+
         setEditorContent(updatedContent);
     };
 
-    const addFieldsIntoContent = ({field_name, display_name}: {field_name: string, display_name: string}) => {
-        // INSERT new content into the editor on the cursor position. Its not a replacement but inserting new content.
-        insertTextAtCursor(`{{${display_name}}}`);
-
+    const addTemplateFieldsIntoEditor = (field: TemplateField) => {
+        // INSERT new content into the editor on the cursor position.
+        insertTextAtCursor(`{{${field.displayNname}}}`);
     }
 
-    const insertTextAtCursor = (text) => {
+    const insertTextAtCursor = (text: string) => {
         const { state, dispatch } = editor.view;
 
         // Create a transaction that inserts the text at the current cursor position
         const transaction = state.tr.insertText(text);
-        console.log('transaction:::: ', transaction)
 
         // Dispatch the transaction to the editor
         dispatch(transaction);
     }
 
-    const fields = [
-        {
-            field_name: 'full_name',
-            display_name: 'Full Name'
-        },
-        {
-            field_name: 'job_position',
-            display_name: 'Job Position'
-        }
-    ]
+    const onClickHandler = () => {
+        replaceTemplateFieldsWithActualData('Full Name', 'Gilbert')
+    }
 
 
   return (
@@ -108,13 +102,13 @@ const CustomEditor = () => {
     <div className="editor">
       {editor && <MenuBar editor={editor} />}
       <EditorContent className="editor__content" editor={editor} />
-      <button className='w-40' onClick={replaceData}>Replace data</button>
+      <button className='w-40' onClick={onClickHandler}>Replace data</button>
     </div>
     <div>
         <h1>Fields</h1>
         <ul>
             {fields.map((field, index) => (
-                <li key={index} onClick={() => addFieldsIntoContent(field)} className='cursor-pointer'>
+                <li key={index} onClick={() => addTemplateFieldsIntoEditor(field)} className='cursor-pointer'>
                     <span>{field.display_name}</span>
                 </li>
             ))}
