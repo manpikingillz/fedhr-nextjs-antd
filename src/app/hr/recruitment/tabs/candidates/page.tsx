f2b47ad2d68b2c0af2f1fa429cd7d4f9b0c017e6
Gilbert Twesigomwe <gilbert@bposeats.com>
2023-09-28 22:38:08 +0300

    #41 add more filter dummy data for candidates

--- src/app/hr/recruitment/tabs/candidates/page.tsx
+++ src/app/hr/recruitment/tabs/candidates/page.tsx
@@ -1,9 +1,29 @@
-'use client'
+'use client';
 
 import React from 'react';
-import { Checkbox, Collapse, CollapseProps, Divider, Dropdown, MenuProps, Rate, Slider, Space, Table } from 'antd';
+import {
+  Checkbox,
+  Collapse,
+  CollapseProps,
+  DatePicker,
+  Divider,
+  Dropdown,
+  MenuProps,
+  Rate,
+  Slider,
+  Space,
+  Table,
+} from 'antd';
 import type { ColumnsType } from 'antd/es/table';
-import { MoreOutlined, SmileOutlined, FileTextFilled, CommentOutlined, MailFilled, ToTopOutlined, DeleteOutlined} from '@ant-design/icons';
+import {
+  MoreOutlined,
+  SmileOutlined,
+  FileTextFilled,
+  CommentOutlined,
+  MailFilled,
+  ToTopOutlined,
+  DeleteOutlined,
+} from '@ant-design/icons';
 import Card from 'antd/es/card/Card';
 
 interface DataType {
@@ -18,51 +38,31 @@ const items: MenuProps['items'] = [
   {
     key: '1',
     icon: <FileTextFilled />,
-    label: (
-      <a>
-        View Resume
-      </a>
-    ),
+    label: <a>View Resume</a>,
   },
   {
     key: '2',
     icon: <CommentOutlined />,
-    label: (
-      <a>
-        Add Comment
-      </a>
-    ),
+    label: <a>Add Comment</a>,
   },
   {
     key: '3',
     icon: <MailFilled />,
-    label: (
-      <a>
-        Send Email
-      </a>
-    ),
+    label: <a>Send Email</a>,
   },
   {
     type: 'divider',
   },
   {
     key: '5',
-    icon: <ToTopOutlined rotate={90}/>,
-    label: (
-      <a>
-        Move Candidate
-      </a>
-    ),
+    icon: <ToTopOutlined rotate={90} />,
+    label: <a>Move Candidate</a>,
   },
   {
     key: '6',
     icon: <DeleteOutlined />,
-    label: (
-      <a>
-        Delete Candidate
-      </a>
-    ),
-  }
+    label: <a>Delete Candidate</a>,
+  },
 ];
 
 const columns: ColumnsType<DataType> = [
@@ -71,13 +71,9 @@ const columns: ColumnsType<DataType> = [
     dataIndex: 'name',
     key: 'name',
     render: (_, item) => (
-     <div>
-        <a>
-          {item.name}
-        </a>
-        <p className='text-gray-500'>
-          {item.address}
-        </p>
+      <div>
+        <a>{item.name}</a>
+        <p className="text-gray-500">{item.address}</p>
       </div>
     ),
   },
@@ -91,9 +87,9 @@ const columns: ColumnsType<DataType> = [
     key: 'status',
     dataIndex: 'status',
     render: (_, item) => (
-    <div>
-      {item.status}
-      <p className='text-gray-500'>Updated in 2 months</p>
+      <div>
+        {item.status}
+        <p className="text-gray-500">Updated in 2 months</p>
       </div>
     ),
   },
@@ -101,19 +97,19 @@ const columns: ColumnsType<DataType> = [
     title: 'Rating',
     dataIndex: 'rating',
     key: 'rating',
-    render: (rating) => <Rate allowHalf defaultValue={2.5} />
+    render: (rating) => <Rate allowHalf defaultValue={2.5} />,
   },
   {
     title: 'Action',
     key: 'action',
     render: (_, item) => (
       <Dropdown menu={{ items }}>
-      <a onClick={(e) => e.preventDefault()}>
-        <Space>
-          <MoreOutlined className='font-bold text-2xl' />
-        </Space>
-      </a>
-    </Dropdown>
+        <a onClick={(e) => e.preventDefault()}>
+          <Space>
+            <MoreOutlined className="font-bold text-2xl" />
+          </Space>
+        </a>
+      </Dropdown>
     ),
   },
 ];
@@ -137,14 +133,13 @@ const data: DataType[] = [
 
 const text = (
   <p style={{ paddingLeft: 24 }}>
-    A dog is a type of domesticated animal. Known for its loyalty and faithfulness, it can be found
-    as a welcome guest in many households across the world.
+    A dog is a type of domesticated animal. Known for its loyalty and
+    faithfulness, it can be found as a welcome guest in many households across
+    the world.
   </p>
 );
 
-const ratingFilter = (
-  <Slider min={0} max={5} range defaultValue={[0, 5]}  />
-)
+const ratingFilter = <Slider min={0} max={5} range defaultValue={[0, 5]} />;
 
 const jobStatusOptions = [
   { value: 'DRAFT', label: 'Draft' },
@@ -155,16 +150,100 @@ const jobStatusOptions = [
 ];
 const jobStatusesFilter = (
   <Checkbox.Group style={{ width: '100%' }}>
-    <div className='flex flex-col'>
-    {
-      jobStatusOptions.map((option) => (
+    <div className="flex flex-col">
+      {jobStatusOptions.map((option) => (
         <Checkbox value={option.value}>{option.label}</Checkbox>
-      ))
-    }
+      ))}
     </div>
   </Checkbox.Group>
-)
+);
 
+const jobOpeningListOptions = [
+  { value: '1', label: 'Software Engineer' },
+  { value: '2', label: 'QA Engineer' },
+  { value: '3', label: 'Automation engineer' },
+  { value: '4', label: 'Backend Engineer' },
+  { value: '5', label: 'UI/UX Designer' },
+];
+const jobOpeningsFilter = (
+  <Checkbox.Group style={{ width: '100%' }}>
+    <div className="flex flex-col">
+      {jobOpeningListOptions.map((option) => (
+        <Checkbox value={option.value}>{option.label}</Checkbox>
+      ))}
+    </div>
+  </Checkbox.Group>
+);
+
+const applicationDatesFilter = (
+  <Checkbox.Group style={{ width: '100%' }}>
+    <div className="flex flex-col">
+      <div className='flex flex-col'>
+        <label>From</label>
+        <DatePicker />
+      </div>
+      <div className='flex flex-col'>
+        <label>To</label>
+        <DatePicker />
+      </div>
+    </div>
+  </Checkbox.Group>
+);
+
+
+const sourcesOptions = [
+  { value: '1', label: 'Glassdoor' },
+  { value: '2', label: 'Indeed' },
+  { value: '3', label: 'Facebook' },
+  { value: '4', label: 'LinkedIn' },
+  { value: '5', label: 'Twitter' },
+  { value: '6', label: 'Zip Recruiter' },
+];
+const sourcesFilter = (
+  <Checkbox.Group style={{ width: '100%' }}>
+    <div className="flex flex-col">
+      {sourcesOptions.map((option) => (
+        <Checkbox value={option.value}>{option.label}</Checkbox>
+      ))}
+    </div>
+  </Checkbox.Group>
+);
+
+const locationsOptions = [
+  { value: '1', label: 'London, UK' },
+  { value: '2', label: 'Kampala, Uganda' },
+  { value: '3', label: 'Nairobi, Kenya' },
+  { value: '4', label: 'Capetown, SA' },
+  { value: '5', label: 'Lagos, Nigeria' },
+  { value: '6', label: 'Kigali, Rwanda' },
+];
+const locationsFilter = (
+  <Checkbox.Group style={{ width: '100%' }}>
+    <div className="flex flex-col">
+      {locationsOptions.map((option) => (
+        <Checkbox value={option.value}>{option.label}</Checkbox>
+      ))}
+    </div>
+  </Checkbox.Group>
+);
+
+const hiringLeadsOptions = [
+  { value: '1', label: 'Gilbert Twesigonwe' },
+  { value: '2', label: 'Eric Tumwine' },
+  { value: '3', label: 'Rosette Katusiime' },
+  { value: '4', label: 'Enock Tayebwa' },
+  { value: '5', label: 'Richard Taremwa' },
+  { value: '6', label: 'John Doe' },
+];
+const hiringLeadsFilter = (
+  <Checkbox.Group style={{ width: '100%' }}>
+    <div className="flex flex-col">
+      {hiringLeadsOptions.map((option) => (
+        <Checkbox value={option.value}>{option.label}</Checkbox>
+      ))}
+    </div>
+  </Checkbox.Group>
+);
 const collapseItems: CollapseProps['items'] = [
   {
     key: '1',
@@ -174,7 +253,7 @@ const collapseItems: CollapseProps['items'] = [
   {
     key: '2',
     label: 'Job Openings',
-    children: text,
+    children: jobOpeningsFilter,
   },
   {
     key: '3',
@@ -184,37 +263,43 @@ const collapseItems: CollapseProps['items'] = [
   {
     key: '4',
     label: 'Application Dates',
-    children: text,
+    children: applicationDatesFilter,
   },
   {
     key: '5',
     label: 'Sources',
-    children: text,
+    children: sourcesFilter,
   },
   {
     key: '6',
     label: 'Job Locations',
-    children: text,
+    children: locationsFilter,
   },
   {
     key: '7',
     label: 'Hiring Leads',
-    children: text,
-  }
+    children: hiringLeadsFilter,
+  },
 ];
 
 const Candidates = () => {
   return (
     <>
-      <div className='flex space-x-6'>
-        <div className='basis-1/4'>
-          <p className='h-10 pl-6 pt-2 bg-stone-500 text-white text-base'>Filter Results</p>
-          <Collapse items={collapseItems} bordered={false} defaultActiveKey={['1']} />
+      <div className="flex space-x-6">
+        <div className="basis-1/4">
+          <p className="h-10 pl-6 pt-2 bg-stone-500 text-white text-base">
+            Filter Results
+          </p>
+          <Collapse
+            items={collapseItems}
+            bordered={false}
+            defaultActiveKey={['1']}
+          />
         </div>
-        <Table columns={columns} dataSource={data} className='basis-3/4' />
+        <Table columns={columns} dataSource={data} className="basis-3/4" />
       </div>
     </>
   );
-}
+};
 
 export default Candidates;
